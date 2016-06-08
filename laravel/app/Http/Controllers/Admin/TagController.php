fd9f9c97bbd849219d70860d2ec9e6e69240c495
Nicolò Veronese <nicveronese@gmail.com>
2016-06-09 00:11:31 +0200

    Tag manager on the admin section
    Added Controller
    Added index view (list)
    Added edit view

---
+++ laravel/app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Tag;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::orderBy('slug', 'ASC')->get();
+
+        return view('admin.tag.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return redirect()->action('Admin\TagController@index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //TODO - Validate the input
+        Tag::create($request->all());
+        return redirect()->action('Admin\TagController@index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return redirect()->action('Admin\TagController@index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tag = Tag::findOrFail($id);
+        return view('admin.tag.edit', compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //TODO - Process the update
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tag = Tag::findOrFail($id);
+        $tag->delete();
+
+        return redirect()->action('Admin\TagController@index');
+    }
+}
---
+++ laravel/resources/views/admin/tag/edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('title', 'Add Tag')
+
+@section('content')
+    
+@endsection
