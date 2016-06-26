96bbf08fe3050318d364b56f55e9b849d19aff24
Nicolò Veronese <nicveronese@gmail.com>
2016-06-26 16:38:55 +0200

    Specs now support a description

--- resources/views/admin/spec/edit.blade.php
+++ resources/views/admin/spec/edit.blade.php
@@ -33,10 +33,17 @@
     </fieldset>
 
     <fieldset class="form-group row">
+        {!! Form::label('description', 'Description:', ['class'=> 'col-sm-2 form-control-label']) !!}
+        <div class="col-sm-10">
+            {!! Form::text('description', null, ['class' => 'form-control', 'placeholder' => 'Description']) !!}
+        </div>
+    </fieldset>
+
+    <fieldset class="form-group row">
         <div class="col-sm-offset-2 col-sm-10">
             <div class="row">
                 <div class="col-sm-1">
-                    <a href="{{ url('admin/spec') }}" class="btn btn-danger">Cancel</a>
+                    <a href="{{ url('admin/specs') }}" class="btn btn-danger">Cancel</a>
                 </div>
                 <div class="col-sm-11">
                     {!! Form::button('Update', ['class' => 'btn btn-success form-control', 'type' => 'submit']) !!}
